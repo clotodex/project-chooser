b283edb78d289b6c67cddd35442f93f7a3d8468d
clotodex <clotodex@online.de>
2020-01-27 21:44:40 +0100

    vX.X.X -> X.X.X

--- make_ebuild.sh
+++ make_ebuild.sh
@@ -10,7 +10,7 @@ sed -i "/^project-chooser-[0-9.]*$/d" "$EBUILD"
 
 # add repo to src_uri
 echo "adding repo to SRC_URI"
-REPO='https://gitlab.com/clotodex/${PN}/-/archive/v${PV}/${PN}-v${PV}.tar.bz2'
+REPO='https://gitlab.com/clotodex/${PN}/-/archive/${PV}/${PN}-${PV}.tar.bz2'
 ESCAPED_REPO="$( echo $REPO | sed -e 's/[\/&]/\\&/g' )"
 sed -i "s/SRC_URI=\"\(.*\)/SRC_URI=\"$ESCAPED_REPO \1/g" "$EBUILD"
 
