d87986c9bc4e687244242820b056ff3139e08db4
clotodex <clotodex@online.de>
2020-01-27 19:16:04 +0100

    a little cleanup and ebuild generator

---
+++ make_ebuild.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "generating ebuild"
+EBUILD="$( cargo ebuild | grep -oP "(?<=Wrote: ).*" )"
+echo "'$EBUILD' generated"
+
+# remove package from crate
+echo "removing package from crates"
+sed -i "/^project-chooser-[0-9.]*$/d" "$EBUILD"
+
+# add repo to src_uri
+echo "adding repo to SRC_URI"
+REPO='https://gitlab.com/clotodex/${PV}/-/archive/v${PV}/${PN}-v${PV}.tar.bz2'
+ESCAPED_REPO="$( echo $REPO | sed -e 's/[\/&]/\\&/g' )"
+sed -i "s/SRC_URI=\"\(.*\)/SRC_URI=\"$ESCAPED_REPO \1/g" "$EBUILD"
+
+# copy alias files
+echo "adding bash alias to bin"
+cat >>"$EBUILD" <<EOL
+src_install() {
+    cargo_src_install --path=.
+
+    dobin "\${FILESDIR}"/project-chooser-bash
+}
+EOL
+echo "done"
